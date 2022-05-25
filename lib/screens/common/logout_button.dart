0d2058c90599337f02015d4ab83cfd161d42c129
amirmojarad <amirmojarad13@gmail.com>
2022-05-25 10:00:11 +0430

    logout button created

---
+++ lib/screens/common/logout_button.dart
@@ -0,0 +1,45 @@
+
+import 'package:flutter/material.dart';
+
+class SubfaveLogoutButton extends StatelessWidget {
+  const SubfaveLogoutButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(decoration:BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(
+                color: Theme.of(context).colorScheme.secondary,
+                width: 3,
+              ),
+              color: Theme.of(context).colorScheme.background,
+            ),
+      child: Material(
+        borderRadius: BorderRadius.circular(16),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(16),
+          hoverColor: Theme.of(context).colorScheme.secondary.withOpacity(0.1),
+          highlightColor: Theme.of(context).colorScheme.primary.withOpacity(0.4),
+          onTap: () {
+            Navigator.pushNamed(context, '/login');
+          },
+          child: SizedBox(
+            width: 200,
+            height: 75,
+            child: Center(
+              child: Text(
+                "Logout",
+                style:
+                    Theme.of(context).textTheme.headlineMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.secondary,
+                        ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
